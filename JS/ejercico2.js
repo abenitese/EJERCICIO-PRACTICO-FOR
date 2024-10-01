0f24a022c30585a84555803a1fbd3f4c7dc6efb0
Alex Benites E. <66579129+abenitese@users.noreply.github.com>
2024-10-01 00:09:06 -0500

    Add files via upload

---
+++ JS/ejercico2.js
@@ -0,0 +1,16 @@
+//ejercicio cuenta regresiva
+for (var i=10;i>=0;i--) {
+    console.log(i);
+}
+
+alert("FELIZ NAVIDAD!");
+console.log("FELIZ NAVIDAD")
+console.log("____________________________________");
+
+//version while
+var cuenta=10;
+while (cuenta>=0) {
+    console.log(cuenta);
+    cuenta--;
+}
+console.log("FELIZ NAVIDAD");
